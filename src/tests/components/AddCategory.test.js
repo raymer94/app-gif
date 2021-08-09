ab75a3f9015b016724996c437a56e9990e50f154
Raymer Rodriguez <ramrodriguez@dgii.gov.do>
2021-08-09 15:05:06 -0400

    update cat

--- src/tests/components/AddCategory.test.js
+++ src/tests/components/AddCategory.test.js
@@ -2,9 +2,18 @@ import { shallow } from "enzyme";
 import AddCategory from "../../Components/AddCatergory";
 
 describe("pruebas en <AddCategory/>", () =>{
-    const wrapper = shallow(<AddCategory/>);
+
+    const setCategory = () =>{};
+    const wrapper = shallow(<AddCategory setCategories={setCategory}/>);
 
     test("debe mostrar el componente", ()=>{
         expect(wrapper).toMatchSnapshot();
+    });
+
+    test("debe de cambiar la caja de texto", () => {
+        const input = wrapper.find('input'); 
+        const value = "hola texto";
+
+        input.simulate('change', { target: { value } });
     });
 })
